28f27961a421c5781db808723859df19d1259c4e
Lianne Haug <lianne@wipro.com>
2019-01-15 15:03:15 -0700

    Add Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <template>
   <div id="app">
     <div id="nav">
-      Nav!
+      <Navbar />
     </div>
     <router-view/>
   </div>
 </template>
+
+
+<script>
+import Navbar from '@/components/Navbar.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Navbar
+  }
+}
+</script>
 
 <style lang="scss">
   @import "assets/styles/base.sass";
---
+++ src/components/Navbar.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class='nav'>
+    <div class='nav-link-container'>
+      <a href='/about'>About</a>
+      <a href='/portfolio'>Portfolio</a>
+      <a href='/contact'>Contact</a>
+    </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'navbar',
+};
+</script>
+
+<style lang="sass">
+  @import "@/assets/styles/base.sass"
+
+  .nav
+    height: 35px
+    padding: 20px
+    background-color: $background
+    color: $bright-blue
+    text-align: center
+    .nav-link-container
+      width: 500px
+      margin-left: auto
+      margin-right: auto
+      a
+        padding-left: 35px
+        padding-right: 35px
+        text-decoration: underline !important
+        &:hover
+          color: $pink
+          opacity: .8
+</style>
